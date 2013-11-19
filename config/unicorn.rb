f8ebb78b7b3d4b139111a34fc0d52fae7e94a275
Adam England <adam.n.england@gmail.com>
2013-11-18 21:53:48 -0600

    Updates for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
@@ -11,7 +11,7 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-end 
+end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
