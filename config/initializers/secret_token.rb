bd3ce2e7a3f0804cb92d1205577454d922633a23
Adam England <adam.n.england@gmail.com>
2013-11-18 22:00:02 -0600

    Adding the secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnodaWeb::Application.config.secret_key_base = '4264c3021a16f8548539d4381a6f575fcb2e61b2493d53a4701e23d54e419119c5fd791d08d72b8d14c09af9ba4b225d4d19f29b7adb19da3b563ed164025367'
