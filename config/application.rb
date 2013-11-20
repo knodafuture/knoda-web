9bdccc0b757cc00466155b01e58fdb092b481510
Adam England <adam.n.england@gmail.com>
2013-11-19 21:25:29 -0600

    Compile the assets

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module KnodaWeb
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += [
+      'jquery.cycle.all.min.js',
+      'rotating_tweet.js',
+      'chunk.css',
+      'rotating_tweets.css'
+    ]    
   end
 end
