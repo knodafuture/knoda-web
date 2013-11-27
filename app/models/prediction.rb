4c279fc427ef2a918748b3121647fec64dcadc83
Adam England <adam.n.england@gmail.com>
2013-11-26 18:36:39 -0600

    Improving prediction share page

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -3,4 +3,16 @@ class Prediction < ActiveRecord::Base
   has_many :challenges, :dependent => :destroy
   has_many :voters, through: :challenges, class_name: "User", source: 'user'
   has_many :comments, :dependent => :destroy
+
+  def disagreed_count
+    self.challenges.find_all_by_agree(false).count
+  end
+
+  def agreed_count
+    self.challenges.find_all_by_agree(true).count
+  end
+
+  def agreePercent
+    ((self.challenges.find_all_by_agree(true).count / self.challenges.count) * 100).round
+  end
 end
