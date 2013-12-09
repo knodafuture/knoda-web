bcf7f46dbab3c9540f6b56a0c7dff274ee252758
Adam England <adam.n.england@gmail.com>
2013-12-08 23:02:22 -0600

    Going from thumb to small

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :predictions, :dependent => :destroy
-  has_attached_file :avatar, :styles => { :big => "344х344>", :small => "100x100>", :thumb => "40x40>" }
+  has_attached_file :avatar, :styles => { :big => "344х344>", :small => "100x100>" }
 
   def avatar_image
     if self.avatar.exists?
