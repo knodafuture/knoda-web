91d1813d921026a5d33d32178c3bd4d8700a6746
Adam England <adam.n.england@gmail.com>
2014-01-23 15:07:59 -0600

    Buffing up the stats for the predictions page

--- app/helpers/predictions_helper.rb
+++ app/helpers/predictions_helper.rb
@@ -1,15 +1,25 @@
 module PredictionsHelper
 	def display_challenge_icon(prediction)
-		c = my_challenge(prediction)
-		if c
-			if c.agree
-				render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userNotChosen', :agreeClass => 'userAgrees'}
+		if (current_user.id != prediction.user_id)
+			c = my_challenge(prediction)
+			if c
+				if c.agree
+					render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userNotChosen', :agreeClass => 'userAgrees'}
+				else
+					render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userDisagrees', :agreeClass => 'userNotChosen'}
+				end
 			else
-				render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userDisagrees', :agreeClass => 'userNotChosen'}
+				puts 'render time'
+				render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userNotChosen', :agreeClass => 'userNotChosen'}
 			end
+		end
+	end
+
+	def display_close_status(prediction)
+		if prediction.expires_at > Time.now
+			return "closes #{distance_of_time_in_words_to_now(prediction.created_at)} from now"
 		else
-			puts 'render time'
-			render :partial => "predictions/display_challenge", :locals => {:disagreeClass => 'userNotChosen', :agreeClass => 'userNotChosen'}
+			return "closed #{distance_of_time_in_words_to_now(prediction.created_at)} ago"
 		end
 	end
 
