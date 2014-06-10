b49104d07c8d80bb50ce8a9718cb196c0070075b
Adam England <adam.n.england@gmail.com>
2014-06-10 13:06:12 -0500

    Bug fixes for embed

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,13 +1,13 @@
 class AuthenticatedController < ApplicationController
-
   before_filter :unseen_activities
   skip_before_filter :verify_authenticity_token
 
   def unseen_activities
+    authenticate_user!
     current_user.predictions.readyForResolution.notAlerted.each do |p|
       p.update(activity_sent_at: DateTime.now)
       current_user.activities.create!(user: current_user, prediction_id: p.id, title: "Your prediction has expired. Please settle the outcome", prediction_body: p.body, activity_type: "EXPIRED");
-    end  	
+    end
   	s = current_user.activities.unseen.size
   	if s > 0
   		@unseen_activities = s
