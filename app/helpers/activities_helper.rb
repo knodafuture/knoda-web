dce1252a721ffe08e00b83c29a7e5861e589f6c7
Adam England <adam.n.england@gmail.com>
2014-07-01 13:31:07 -0500

    Properly colorize won/lost text for activities

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -13,5 +13,17 @@ module ActivitiesHelper
         "icons/activity/activity_groups_icon.png"
     end
   end
-
+  def activity_title(activity)
+    if activity.activity_type == 'WON'
+      title = activity.title
+      title.gsub!("You Won", "<span class='won-text'>You Won</span>")
+      return "#{title}"
+    elsif activity.activity_type == 'LOST'
+      title = activity.title
+      title.gsub!("You Lost", "<span class='lost-text'>You Lost</span>")
+      return "#{title}"
+    else
+      return activity.title
+    end
+  end
 end
