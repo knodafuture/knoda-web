41557aa924a115d93394f0eeec7c9f82d89e14a2
Adam England <adam.n.england@gmail.com>
2014-07-01 14:19:53 -0500

    Pixel pushing with Luke B
    More
    
    More
    
    More
    
    More tweaks
    
    Bumping core

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,16 +1,8 @@
 module ActivitiesHelper
   def activity_icon(activity)
 	case (activity.activity_type)
-      when 'LOST'
-        activity.image_url
-      when 'WON'
-          activity.image_url
-      when 'COMMENT'
-        "icons/ActivityCommentIcon.png"
       when 'EXPIRED'
-        "icons/ActivityExpiredIcon.png"
-      when "INVITATION"
-        "icons/activity/activity_groups_icon.png"
+        "icons/ActivityExpiredIcon@2x.png"
     end
   end
   def activity_title(activity)
