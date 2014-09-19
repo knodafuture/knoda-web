becfe39647fb0a2fe62db4adc582ac9af07a35b8
Adam England <adam.n.england@gmail.com>
2014-09-19 10:30:34 -0500

    Pixel pushing
    Pixel pushing
    
    Pixel pushing
    
    Pixel pushing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -210,11 +210,45 @@ module ApplicationHelper
     return opponentPercentage
   end
 
+  def vs_my_bar_width(vs)
+    if vs[:opponent_won] == 0
+      myPercentage = 100
+    else
+      myPercentage = vs[:user_won].fdiv((vs[:user_won] + vs[:opponent_won]))* 100
+    end
+    return myPercentage
+  end
+
+  def vs_opponent_bar_width(vs)
+    if vs[:opponent_won] == 0
+      opponentPercentage = 100
+    else
+      opponentPercentage = vs[:opponent_won].fdiv((vs[:user_won] + vs[:opponent_won]))* 100
+    end
+    return opponentPercentage
+  end
+
   def win_percentage(user)
     if (user.won + user.lost) > 0
       return "#{(user.won.fdiv(user.won + user.lost) * 100).round(2)}%"
     else
       return "0%"
+    end
+  end
+
+  def win_percentage_status(user1, user2)
+    if (user1.winning_percentage > user2.winning_percentage)
+      return "winner"
+    else
+      return "loser"
+    end
+  end
+
+  def streak_status(user1, user2)
+    if (user1.streak > user2.streak)
+      return "winner"
+    else
+      return "loser"
     end
   end
 end
