4f6830e7535125df3e4b5291f45c16d8c6fe241d
Adam England <adam.n.england@gmail.com>
2014-10-01 16:45:54 -0500

    Fixing login bug

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -3,15 +3,12 @@ class AuthenticatedController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def unseen_activities
-    puts "RUNNING UNSEEN"
     authenticate_user!
     current_user.predictions.readyForResolution.notAlerted.each do |p|
       p.update(activity_sent_at: DateTime.now)
       current_user.activities.create!(user: current_user, prediction_id: p.id, title: "Showtime! Your prediction has expired, settle it.", prediction_body: p.body, activity_type: "EXPIRED");
     end
   	s = current_user.activities.unseen.size
-    puts "UNSEEN"
-    puts s
   	if s > 0
   		@unseen_activities = s
   	end
